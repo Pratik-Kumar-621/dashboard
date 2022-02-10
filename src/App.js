42b6568fc6c1c314436b09fca067b6dd9bc21b9c
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2022-02-10 15:53:49 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import DashBoard from "./screens/DashBoard/DashBoard";
+import AddPosts from "./screens/AddPosts/AddPosts";
+import BlogPost from "./screens/BlogPost/BlogPost";
+import Error from "./screens/Errors/Error";
+import FormsAndComponents from "./screens/FormsAndComponents/FormsAndComponents";
+import Profile from "./screens/Profile/Profile";
+import Tables from "./screens/Tables/Tables";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <DashBoard />
+        </Route>
+        <Route exact path="/blog-posts">
+          <BlogPost />
+        </Route>
+        <Route exact path="/errors">
+          <Error />
+        </Route>
+        <Route exact path="/form-and-component">
+          <FormsAndComponents />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/table">
+          <Tables />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/screens/FormsAndComponents/FormsAndComponents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormsAndComponents = () => {
+  return <div>FormsAndComponents</div>;
+};
+
+export default FormsAndComponents;
