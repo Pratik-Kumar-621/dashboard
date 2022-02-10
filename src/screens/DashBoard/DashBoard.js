cc1433f241af2e012c175d4200b1d207a9afafb9
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2022-02-10 21:14:53 +0530

    Data Card

--- src/screens/DashBoard/DashBoard.js
+++ src/screens/DashBoard/DashBoard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import DataCard from "../../Components/DashBoard/DataCard/DataCard";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  return (
+    <div>
+      <DataCard />
+    </div>
+  );
 };
 
 export default DashBoard;
