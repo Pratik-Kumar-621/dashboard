5f6b8f67b6a38a7dcc6dd0ffacad0f924f3d9466
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2022-02-10 21:31:40 +0530

    Finished

--- src/screens/DashBoard/DashBoard.js
+++ src/screens/DashBoard/DashBoard.js
@@ -1,10 +1,17 @@
 import React from "react";
 import DataCard from "../../Components/DashBoard/DataCard/DataCard";
+import OverViewGraph from "../../Components/DashBoard/OverviewGraph/OverViewGraph";
+import UserGraph from "../../Components/DashBoard/UserGraph/UserGraph";
+import "./DashBoard.scss";
 
 const DashBoard = () => {
   return (
     <div>
       <DataCard />
+      <div className="images-graphs">
+        <OverViewGraph />
+        <UserGraph />
+      </div>
     </div>
   );
 };
